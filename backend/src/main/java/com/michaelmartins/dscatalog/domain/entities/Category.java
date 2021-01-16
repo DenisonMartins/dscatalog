d2c8beafefcbfecfa0bff5efa1d1037f24c70709
Michael <michael.dl.mrtns@gmail.com>
2021-01-16 02:08:28 -0300

    WIP: Criação da entity Category

---
+++ backend/src/main/java/com/michaelmartins/dscatalog/domain/entities/Category.java
@@ -0,0 +1,57 @@
+package com.michaelmartins.dscatalog.domain.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id.equals(category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
