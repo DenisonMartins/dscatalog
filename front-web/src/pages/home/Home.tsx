8e64b2ec117ba2aa6641f9ebed0cd944896b900e
Michael <michael.dl.mrtns@gmail.com>
2021-01-23 10:46:26 -0300

    Add Home

--- front-web/src/pages/home/Home.tsx
+++ front-web/src/pages/home/Home.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { ReactComponent as MainImage} from '../../core/assets/images/main-image.svg';
 import './styles.scss';
+import ButtonIcon from "../../core/components/ButtonIcon/ButtonIcon";
 
 const Home = () => (
     <div className="home-container">
-        <h1>Home</h1>
+        <div className="row home-content">
+            <div className="col-6">
+                <h1 className="text-title">
+                    Conheça o melhor <br /> catálogo de produtos
+                </h1>
+                <p className="text-subtitle">
+                    Ajudaremos você a encontrar os melhores <br/> produtos disponíveis no mercado.
+                </p>
+                <ButtonIcon text="inicie agora sua busca"/>
+            </div>
+            <div className="col-6">
+                <MainImage className="main-image"/>
+            </div>
+        </div>
     </div>
 )
 
