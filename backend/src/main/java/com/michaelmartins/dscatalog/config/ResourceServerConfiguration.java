320f90026eed6f7b917d5de3c7d6c3057199346d
Michael <michael.dl.mrtns@gmail.com>
2021-02-19 14:12:56 -0300

    h2 security configuration

--- backend/src/main/java/com/michaelmartins/dscatalog/config/ResourceServerConfiguration.java
+++ backend/src/main/java/com/michaelmartins/dscatalog/config/ResourceServerConfiguration.java
@@ -1,6 +1,7 @@
 package com.michaelmartins.dscatalog.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -8,18 +9,24 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import static java.util.Arrays.asList;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
-    private final JwtTokenStore tokenStore;
-
-    private static final String[] PUBLIC = { "/oauth/login" };
+    public static final String PROFILE_TEST = "test";
+    private static final String[] PUBLIC = { "/oauth/login", "/h2-console/**" };
     private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
     private static final String[] ADMIN = { "/users/**" };
 
-    public ResourceServerConfiguration(JwtTokenStore tokenStore) {
+    private final JwtTokenStore tokenStore;
+    private final Environment env;
+
+    public ResourceServerConfiguration(JwtTokenStore tokenStore,
+                                       Environment env) {
         this.tokenStore = tokenStore;
+        this.env = env;
     }
 
     @Override
@@ -29,11 +36,20 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        verificaSeDeveLiberarBancoDadosLocalH2ParaProfileTest(http);
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
                 .antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
                 .antMatchers(ADMIN).hasRole("ADMIN")
                 .anyRequest().authenticated();
+    }
+
+    private void verificaSeDeveLiberarBancoDadosLocalH2ParaProfileTest(HttpSecurity http) throws Exception {
+        if (asList(env.getActiveProfiles()).contains(PROFILE_TEST)) {
+            http.headers().frameOptions().disable();
+        }
     }
 }
