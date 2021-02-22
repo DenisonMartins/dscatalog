6ce622d44293eda38bb2637155f6d6dd36107ccb
Michael <michael.dl.mrtns@gmail.com>
2021-02-22 09:30:09 -0300

    cors configuration

--- backend/src/main/java/com/michaelmartins/dscatalog/config/ResourceServerConfiguration.java
+++ backend/src/main/java/com/michaelmartins/dscatalog/config/ResourceServerConfiguration.java
@@ -1,15 +1,24 @@
 package com.michaelmartins.dscatalog.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 import static java.util.Arrays.asList;
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableResourceServer
@@ -41,15 +50,38 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
-                .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
+                .antMatchers(GET, OPERATOR_OR_ADMIN).permitAll()
                 .antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
                 .antMatchers(ADMIN).hasRole("ADMIN")
                 .anyRequest().authenticated();
+
+        http.cors().configurationSource(corsConfigurationSource());
     }
 
     private void verificaSeDeveLiberarBancoDadosLocalH2ParaProfileTest(HttpSecurity http) throws Exception {
         if (asList(env.getActiveProfiles()).contains(PROFILE_TEST)) {
             http.headers().frameOptions().disable();
         }
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
+        corsConfiguration.setAllowedMethods(asList(GET.name(), POST.name(), PUT.name(), PATCH.name(), DELETE.name()));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedHeaders(asList("Authorization","Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean =
+                new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        corsFilterFilterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return corsFilterFilterRegistrationBean;
     }
 }
